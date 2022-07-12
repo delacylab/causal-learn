651876d10dff0770cb78f82b87da9732cd12325c
delacylab <93732324+delacylab@users.noreply.github.com>
2022-07-12 16:12:25 -0600

    Update BackgroundKnowledgeOrientUtils.py
    Changed remove_edge to remove_edge_reconstitute_dpath

--- causallearn/utils/PCUtils/BackgroundKnowledgeOrientUtils.py
+++ causallearn/utils/PCUtils/BackgroundKnowledgeOrientUtils.py
@@ -24,14 +24,14 @@ def orient_by_background_knowledge(cg: CausalGraph, background_knowledge: Backgr
     for edge in cg.G.get_graph_edges():
         if cg.G.is_undirected_from_to(edge.get_node1(), edge.get_node2()):
             if background_knowledge.is_forbidden(edge.get_node2(), edge.get_node1()):
-                cg.G.remove_edge(edge)
+                cg.G.remove_edge_reconstitute_dpath(edge)
                 cg.G.add_directed_edge(edge.get_node1(), edge.get_node2())
             elif background_knowledge.is_forbidden(edge.get_node1(), edge.get_node2()):
-                cg.G.remove_edge(edge)
+                cg.G.remove_edge_reconstitute_dpath(edge)
                 cg.G.add_directed_edge(edge.get_node2(), edge.get_node1())
             elif background_knowledge.is_required(edge.get_node2(), edge.get_node1()):
-                cg.G.remove_edge(edge)
+                cg.G.remove_edge_reconstitute_dpath(edge)
                 cg.G.add_directed_edge(edge.get_node2(), edge.get_node1())
             elif background_knowledge.is_required(edge.get_node1(), edge.get_node2()):
-                cg.G.remove_edge(edge)
+                cg.G.remove_edge_reconstitute_dpath(edge)
                 cg.G.add_directed_edge(edge.get_node1(), edge.get_node2())
